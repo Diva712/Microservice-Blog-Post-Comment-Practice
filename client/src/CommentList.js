2c78c88e41ffc2b1733db1bafebbf4901d1cee5e
Diva gupta <divagupta712@gmail.com>
2024-07-07 16:41:09 +0530

    micro-service-practise

---
+++ client/src/CommentList.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+
+
+const CommentList = ({ comments }) => {
+
+
+
+  const renderedComment = comments.map(comment => {
+    let content;
+    if (comment.status === 'approved') {
+      content = comment.content;
+    }
+
+    if (comment.status === 'pending') {
+      content = 'This comment is awaiting moderation';
+    }
+
+    if (comment.status === 'rejected') {
+      content = 'This comment has been rejected'
+    }
+    return <li key={comment.id}>{content}</li>
+  })
+  return (
+    <ul>
+      {renderedComment}
+    </ul>
+  )
+}
+
+export default CommentList

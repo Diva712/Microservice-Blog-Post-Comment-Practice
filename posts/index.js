28ea3119c26ab50dcc176d1f4c4f3f4141615141
Diva gupta <divagupta712@gmail.com>
2024-07-15 19:11:57 +0530

    adding k8 configuration in post and event

--- posts/index.js
+++ posts/index.js
@@ -25,7 +25,7 @@ app.post('/posts', async (req, res) => {
     };
 
     //send event to event-bus
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv/:4005/events', {
       type: 'PostCreated',
       data: {
         id,
@@ -48,6 +48,7 @@ app.post('/events', (req, res) => {
 })
 
 app.listen(4000, () => {
+  console.log("v990");
   console.log("Listening at 4000")
 })
 
